bf2210e828176243524e36c22c0d14e4c920f695
kazma <kazmatw@gmail.com>
2025-03-01 08:43:26 +0800

    Fix struct

--- demo_file/struct.c
+++ demo_file/struct.c
@@ -2,15 +2,15 @@
 #include <string.h>
 
 struct Employee {
-    char name[50];
+    char name[10];
     int id;
     float salary;
 };
 
 void printEmployee(struct Employee emp) {
-    printf("員工姓名: %s\n", emp.name);
-    printf("員工編號: %d\n", emp.id);
-    printf("薪資: %.2f\n", emp.salary);
+    printf("Name: %s\n", emp.name);
+    printf("ID: %d\n", emp.id);
+    printf("Salary: %.2f\n", emp.salary);
 }
 
 int main() {
@@ -18,4 +18,3 @@ int main() {
     printEmployee(emp1);
     return 0;
 }
-
